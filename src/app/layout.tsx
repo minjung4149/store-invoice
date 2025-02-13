270e832aad5aff330ef857f6bf159cd82e8f63df
“MinjungSeo” <mjseo@notiplus.co.kr>
2025-02-13 09:56:14 +0900

    init commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+// import '@/styles/style.scss';
+
+export default function RootLayout({children}: { children: React.ReactNode }) {
+
+  return (
+    <html lang="ko">
+    <head>
+      <title>중앙청과 20번 계산서</title>
+      <meta name="robots" content="noindex"/>
+      <meta name="keywords"
+            content=""/>
+      <meta name="description"
+            content=""/>
+    </head>
+    <body>
+    {children}
+    </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+    중앙청과
+    </>
+  );
+}
