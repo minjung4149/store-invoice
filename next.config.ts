19f997fe67b7c416eded72b2fe587ee5ae9d5763
“MinjungSeo” <mjseo@notiplus.co.kr>
2025-03-12 17:04:06 +0900

    빌드에러 해결

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true, // React의 Strict Mode 활성화 (디버깅 도움)
-  output: undefined, //  `standalone` 설정이 있으면 비활성화
+  reactStrictMode: true, // React Strict Mode 활성화 (디버깅 도움)
+  experimental: {}, // 빈 객체로 설정하여 `serverActions` 오류 방지
 };
 
 export default nextConfig;
